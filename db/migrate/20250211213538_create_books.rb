cec0ba8fcc74efd76b2d913d4bba65fec8446f30
Seko Reinhard <rbonnke@innovexsolutions.co.ke>
2025-02-12 01:13:15 +0300

    feat: added a book resource,implemented validation at the model and migrations level

---
+++ db/migrate/20250211213538_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :books do |t|
+      t.string :title, null: false
+      t.string :author, null: false
+      t.string :isbn, null: false
+      t.boolean :available
+
+      t.timestamps
+    end
+    add_index :books, :isbn, unique: true
+  end
+end
